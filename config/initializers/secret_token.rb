ea9da7bf735015e4862f84d822036387a3bb8efd
Matias Reyes <matreyes@gmail.com>
2011-11-07 10:56:34 -0300

    course Model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cursouno::Application.config.secret_token = 'b440fca5e3f8dacf6d6b529e198f632b4bc7f1f3b13a5fdfe16313d28ccc85c46e9acbd20e51610f812fa7af3ba9cfff5238889980b2c794081f010581e80417'

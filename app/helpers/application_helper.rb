12b869266037b94151a812c148b3ef3b06a48a31
Matias Reyes <matreyes@gmail.com>
2011-12-14 20:27:06 -0300

    tutor block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
  
-  def tutor_block
-      if current_user && current_user.tutor
-        content_tag :div, current_user.tutor.email, class: 'tutor'
-      end
-  end
-
 end

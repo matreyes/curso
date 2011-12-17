7cd5afa3984cf978d5f2698fea11856ad22fc951
Fernando Blat <ferblape@gmail.com>
2011-12-17 16:49:37 -0300

    Truncate the content of a comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,12 @@
 $(document).ready(function(){
 	$("body").click(function(){
 		$("#flash_messages").fadeOut();
-	})
+	});
+	$('.view_full_comment').on('click', function(e){
+	  var parent = $(this).parents('div.well');
+	  parent.find("div.on").toggle();
+	  parent.find("div.off").toggle();
+	  e.preventDefault(); e.stopPropagation();
+	});
 });
 

5582ad492051fceeedc03798457daa75f179f776
Matias Reyes <matreyes@gmail.com>
2011-12-18 20:09:59 -0300

    Read CSV and validate passport

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# coding: UTF-8
+
 # == Schema Information
 #
 # Table name: users
@@ -23,7 +25,12 @@
 #  updated_at             :datetime
 #
 
+require 'open-uri'
+require 'csv'
+
 class User < ActiveRecord::Base
+  
+  CSV_URL = "https://docs.google.com/spreadsheet/pub?hl=es&hl=es&key=0Apc-qBdqkJwVdGJMUXpRYS00RTdZZVU3cTN0VWc2YVE&single=true&gid=0&output=csv"
   
   has_many :participants, class_name: 'User'
   belongs_to :tutor, class_name: 'User'
@@ -49,6 +56,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, 
                   :current_level, :tutor_id, :passport, :name, :surname, :avatar
   
+  before_validation :check_passport_number
+  
   # will_paginate
   self.per_page = 20
   
@@ -60,8 +69,34 @@ class User < ActiveRecord::Base
     if level == current_level+1 
       self.update_attributes(current_level: level)
     end
-    
   end
   
+  private
   
+  def check_passport_number
+    return unless new_record?
+    
+    found = false
+    read_csv.each do |row|
+      if row[0] == self.passport.to_s
+        found = true
+        self.tutor_id = row[1]
+        self.is_admin = row[2] == '1' ? true : false
+      end
+    end
+    unless found
+      errors.add(:passport, "no se encuentra registrado.")
+    end
+  end
+  
+  def read_csv
+    data = []
+    begin
+      open(CSV_URL) do |f|
+        data = CSV.parse(f)
+      end
+    rescue IOError => e
+    end
+    return data
+  end
 end

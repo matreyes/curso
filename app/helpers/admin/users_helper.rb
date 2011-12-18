7912768e5785a49b34c06685bcb7c8c6c685d248
Matias Reyes <matreyes@gmail.com>
2011-12-18 16:59:49 -0300

    list of users responses OK

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,12 @@
 module Admin::UsersHelper
+  
+  def quiz_answer(quiz, user)
+    answer = quiz.answers.where(:user_id => user.id).first
+    if answer.blank?
+      '<span class="label important">ko</span>'.html_safe
+    else
+      '<span class="label success">ok</span>'.html_safe
+    end
+  end
+  
 end

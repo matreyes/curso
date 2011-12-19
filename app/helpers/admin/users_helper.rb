c7ab30f37b656ca6c4e88227a5777d651c1fa756
Matias Reyes <matreyes@gmail.com>
2011-12-19 20:25:36 -0300

    timeline on levels

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,16 +1,17 @@
 module Admin::UsersHelper
   
-  def quiz_answer(quiz, user)
+  def admin_quiz_answer(quiz, user)
     answer = quiz.answers.where(:user_id => user.id).first
     if answer.blank?
-      '<span class="label important">ko</span>'.html_safe
+      "<span class='label'>P#{quiz.id}</span>".html_safe
     else
-      if user.checked_at.blank? || user.checked_at > answer.updated_at
-        '<span class="label success">ok</span>'.html_safe
+      if !user.checked_at.blank? && user.checked_at > answer.updated_at
+        "<span class='label success'>P#{quiz.id}</span>".html_safe
       else
-        '<span class="label warning">rev</span>'.html_safe
+        "<span class='label warning'>P#{quiz.id}</span>".html_safe
       end
     end
   end
+
   
 end
--- app/helpers/levels_helper.rb
+++ app/helpers/levels_helper.rb
@@ -1,2 +1,19 @@
 module LevelsHelper
+  
+  def quiz_answer(level, quiz, user)
+    answer = quiz.answers.where(:user_id => user.id).first
+    if answer.blank?
+      # Blank
+      "<span class='label'>P#{quiz.id}</span>".html_safe
+    else
+      if !user.checked_at.blank? && user.checked_at > answer.updated_at
+        # OK
+        link_to "<span class='label success'>P#{quiz.id}</span>".html_safe, level_quiz_path(level, quiz)
+      else
+        # Waiting for feedback
+        link_to "<span class='label warning'>P#{quiz.id}</span>".html_safe, level_quiz_path(level, quiz)
+      end
+    end
+  end
+  
 end

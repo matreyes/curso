d0923c2f77779c7e834ced18848fa8b03b03051c
Fernando Blat <ferblape@gmail.com>
2011-12-20 20:35:52 -0300

    Prepared to deploy!

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'devise'
 gem 'rmagick'
 gem 'will_paginate'
 gem 'carrierwave'
-
+gem 'mysql2'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ config/deploy.rb
@@ -0,0 +1,73 @@
+require "bundler/capistrano"
+
+default_run_options[:pty] = true
+
+set :application, 'curso'
+set :branch, "master"
+set :scm, :git
+set :git_shallow_clone, 1
+set :scm_user, 'pasa'
+set :use_sudo, false
+set :repository, "git://github.com/matreyes/curso.git"
+ssh_options[:forward_agent] = true
+set :keep_releases, 5
+
+set :server, '190.54.16.190'
+
+set :user,  'pasa'
+set :port, "22"
+
+set(:deploy_to){
+  "/home/pasa/curso"
+}
+
+before "db:setup", "deploy:update_code", "bundle:install"
+after "deploy:update_code", :symlinks
+after "deploy:symlink", :run_migrations
+# after "deploy:stop",    "delayed_job:stop"
+# after "deploy:start",   "delayed_job:start"
+# after "deploy:restart", "delayed_job:stop"
+after "deploy", "deploy:cleanup"
+
+desc "Restart Application"
+deploy.task :restart, :roles => [:app] do
+  run "touch #{current_path}/tmp/restart.txt"
+end
+
+desc "Migraciones"
+task :run_migrations, :roles => [:app] do
+  run <<-CMD
+    export RAILS_ENV=production &&
+    cd #{release_path} &&
+    rake db:migrate --trace
+  CMD
+end
+
+task :symlinks, :roles => [:app] do
+  run <<-CMD
+    ln -s #{shared_path}/system #{release_path}/public/system;
+    ln -s #{shared_path}/cache #{release_path}/public/;
+    ln -s #{shared_path}/uploads #{release_path}/public/;
+  CMD
+end
+
+# namespace :delayed_job do
+#   def rails_env
+#     fetch(:rails_env, false) ? "RAILS_ENV=#{stage}" : ''
+#   end
+#
+#   desc "Stop the delayed_job process"
+#   task :stop, :roles => :app do
+#     run "cd #{current_path};#{rails_env} script/delayed_job stop"
+#   end
+#
+#   desc "Start the delayed_job process"
+#   task :start, :roles => :app do
+#     run "cd #{current_path};#{rails_env} script/delayed_job start"
+#   end
+#
+#   desc "Restart the delayed_job process"
+#   task :restart, :roles => :app do
+#     run "cd #{current_path}; #{rails_env} script/delayed_job restart"
+#   end
+# end

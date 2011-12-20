84655ff7fe30062959b4c9bfb3b3fabda5bc980c
Matias Reyes <matreyes@gmail.com>
2011-12-20 00:34:54 -0300

    Our favorite icons for quizzes states are now in the quiz itself!

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,17 +1,4 @@
 module Admin::UsersHelper
-  
-  def admin_quiz_answer(quiz, user)
-    answer = quiz.answers.where(:user_id => user.id).first
-    if answer.blank?
-      "<span class='label'>P#{quiz.id}</span>".html_safe
-    else
-      if !user.checked_at.blank? && user.checked_at > answer.updated_at
-        "<span class='label success'>P#{quiz.id}</span>".html_safe
-      else
-        "<span class='label warning'>P#{quiz.id}</span>".html_safe
-      end
-    end
-  end
 
   
 end
--- app/helpers/levels_helper.rb
+++ app/helpers/levels_helper.rb
@@ -8,10 +8,23 @@ module LevelsHelper
     else
       if !user.checked_at.blank? && user.checked_at > answer.updated_at
         # OK
-        link_to "<span class='label success'>P#{quiz.id}</span>".html_safe, level_quiz_path(level, quiz)
+        levellink_to "<span class='label success'>P#{quiz.id}</span>".html_safe, level_quiz_path(level, quiz)
       else
         # Waiting for feedback
         link_to "<span class='label warning'>P#{quiz.id}</span>".html_safe, level_quiz_path(level, quiz)
+      end
+    end
+  end
+  
+  def no_link_quiz_answer(quiz, user)
+    answer = quiz.answers.where(:user_id => user.id).first
+    if answer.blank?
+      "<span class='label'>P#{quiz.id}</span>".html_safe
+    else
+      if !user.checked_at.blank? && user.checked_at > answer.updated_at
+        "<span class='label success'>P#{quiz.id}</span>".html_safe
+      else
+        "<span class='label warning'>P#{quiz.id}</span>".html_safe
       end
     end
   end

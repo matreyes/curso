3c2669ba56696ad96eb3be6fe8f9a605d6af9c0e
Matias Reyes <matreyes@gmail.com>
2011-12-21 18:33:24 -0300

    seeds and adjustments for quizzes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,27 +9,58 @@ levels = Level.create([
   ])
 
 quizes = Quiz.create([
-  { level_id: 1, question: '¿Qué te parece todo esto?', instant_feedback: 'recuerda que te tienes que portar bien' },
-  { level_id: 1, question: 'Aquí hay algo más...', instant_feedback: 'súper' },
-  { level_id: 2, question: '¿y esto que tal?', instant_feedback: 'Muy bien' }
+  { level_id: 1, 
+    question: 'Comente libremente cuáles son sus expectativas con respecto al curso.', 
+    instant_feedback: '' },
+  { level_id: 2, 
+    question: '¿Porqué Isabel toma esta decisión en el acto 1?', 
+    instant_feedback: '' },
+  { level_id: 2, 
+    question: '¿Porqué Isabel toma esta decisión en el acto 3?', 
+    instant_feedback: '' },
+  { level_id: 2, 
+    question: 'Comparando ambas respuestas. ¿Cuál es el cambio interno que experimenta Isabel?', 
+    instant_feedback: '' },
+  { level_id: 2, 
+    question: 'Desarrolle un acto 2 escrito en primera persona (como si usted fuera Isabel) que haga coherente su proceso de cambio', 
+    instant_feedback: '' },
+  { level_id: 3, 
+    question: 'En términos generales ¿Qué concluye sobre los cambios generacionales que se han producido en torno a la sexualidad y afectividad?', 
+    instant_feedback: '' },
+  { level_id: 3, 
+    question: '¿Qué puede decir sobre las fuentes con que las personas nos informamos sobre fenómenos relacionados a la sexualidad y afectividad?', 
+    instant_feedback: '' },
+  { level_id: 3, 
+    question: '¿En qué consiste el concepto de Sexualidad Protegida?', 
+    instant_feedback: '' },
+  { level_id: 3, 
+    question: '¿Cuál es su opinión sobre la abstinencia como objetivo para la educación sexual?', 
+    instant_feedback: '' },
+  { level_id: 3, 
+    question: '¿Qué elementos del momento informativo le han sido útiles para comprender mejor a Isabel?', 
+    instant_feedback: '' },
+  { level_id: 4, 
+    question: '¿Porqué Isabel toma esta decisión en el acto 1?', 
+    instant_feedback: '' },
+  { level_id: 4, 
+    question: '¿Porqué Isabel toma esta decisión en el acto 3?', 
+    instant_feedback: '' },
+  { level_id: 4, 
+    question: 'Comparando ambas respuestas ¿Cuál es el cambio interno que experimenta Isabel?', 
+    instant_feedback: '' },
+  { level_id: 4, 
+    question: 'Desarrolle un acto 2 escrito en primera persona (como si usted fuera Isabel) que haga coherente su proceso de cambio', 
+    instant_feedback: '' },
+  { level_id: 5, 
+    question: 'Comparando su primera y segunda resolución del Caso de Aprendizaje. ¿Qué aprendizajes personales identifica?', 
+    instant_feedback: '' },
+  { level_id: 5, 
+    question: 'Comente libremente como evalúa la experiencia de haber realizado este curso.', 
+    instant_feedback: '' }
   ])
   
 topics = Topic.create([
-  { name: "tema1", abstract: "este es el tema 1"},
-  { name: "tema2", abstract: "este es el tema 2"}
+  { name: "Hablemos de cualquier cosa", abstract: "En este foro se puede conversar de los temas que le interese con sus compañeros(as)." },
+  { name: "Hablemos de sexualidad y afectividad", abstract: "Este foro es para conversar sobre sexualidad, afectividad y temas relacionados con el curso." },
+  { name: "Soporte Técnico", abstract: "¿Problemas con el curso? Coméntelas aquí y buscaremos una solución." }
   ])  
-comments = Comment.create([
-  { topic_id: 1, user_id: 1, text: "texto 1 del tema 1"},
-  { topic_id: 1, user_id: 2, text: "texto 2 del tema 1"},
-  { topic_id: 1, user_id: 1, text: "texto 3 del tema 1"},
-  { topic_id: 2, user_id: 2, text: "texto 1 del tema 2"},
-  { topic_id: 2, user_id: 2, text: "texto 2 del tema 1"}
-  ])
-
-  
-  
-admin = User.new(id:1, name: 'Administrador', email: 'admin@mail.com', password: '1234', password_confirmation: '1234')
-admin.is_admin = true
-admin.save
-
-user = User.create(id: 2, name: 'Usuario', email: 'user@mail.com', password: '1234', password_confirmation: '1234', tutor_id: 1)

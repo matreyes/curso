1ebefd1afd133a72b06fcfaa0d6b42c51d7735a3
Matias Reyes <matreyes@gmail.com>
2011-12-29 18:04:29 -0300

    Changed mailer subject tag to filter.

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -2,7 +2,7 @@ class MainMailer < ActionMailer::Base
 
   def send_mail(user, to_email, message)
     @message = message
-    mail(to: to_email, from: user.email, subject: "[Mensaje 24+5] de: #{user}")
+    mail(to: to_email, from: user.email, subject: "[Mensaje]: #{user}")
   end
 
 

cabdcb50a22fcf1c4cfbb4df36c00a07e71ed638
Matias Reyes <matreyes@gmail.com>
2011-12-29 23:53:15 -0300

    RAND() for production, RANDOM() for development and test

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,12 @@ class TopicsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = User.where("avatar IS NOT NULL AND id IS NOT ?", current_user.id).order("RANDOM()").limit(20)
+    if Rails.env == "production"
+      random = "RAND()"
+    else
+      random = "RANDOM()"
+    end
+    @users = User.where("avatar IS NOT NULL AND id IS NOT ?", current_user.id).order(random).limit(20)
     @topics = Topic.all
   end
 

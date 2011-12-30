9e431b31580d5eea1649845317c8cdc6de61bb08
Matias Reyes <matreyes@gmail.com>
2011-12-30 00:10:50 -0300

    BUGFIX: Mysql statement

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,7 @@ class TopicsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = User.where("avatar IS NOT NULL AND id IS NOT ?", current_user.id).limit(60).sort_by{ rand }
+    @users = User.where("avatar IS NOT NULL AND id <> ?", current_user.id).limit(60).sort_by{ rand }
     @topics = Topic.all
   end
 

9e9a03062e4e3500fc890efec112b8a6bf3fc80a
Fernando Blat <ferblape@gmail.com>
2012-01-05 20:21:25 -0300

    Fixxxxx

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,11 +3,11 @@ class TopicsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    MAX = 66
-    offset = User.with_avatar.count - MAX
+    max = 66
+    offset = User.with_avatar.count - max
     start_in = offset < 0 ? 0 : rand(offset)
 
-    @users = User.with_avatar.limit(MAX).offset(start_in).sort_by{ rand }
+    @users = User.with_avatar.limit(max).offset(start_in).sort_by{ rand }
     @topics = Topic.all
   end
 

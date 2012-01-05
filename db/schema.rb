e3b672aad3ef67b16bf3768375e92ff9a69ec450
Fernando Blat <ferblape@gmail.com>
2012-01-05 19:20:29 -0300

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111227161734) do
+ActiveRecord::Schema.define(:version => 20111229210716) do
 
   create_table "answers", :force => true do |t|
     t.integer  "quiz_id"

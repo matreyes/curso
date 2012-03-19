b7ebc00af40a7a5ebdef9e20788b7f740a7e266c
Matias Reyes <matreyes@gmail.com>
2012-03-19 01:03:05 -0300

    default sender to avoid marked as a spam

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -1,5 +1,7 @@
 class MainMailer < ActionMailer::Base
-
+  
+  default :sender => 'mensajes@pasa.cl'
+  
   def send_mail(user, to_email, message)
     @message = message
     mail(to: to_email, from: user.email, subject: "[Mensaje]: #{user}")
